b8cfeb0bda0a7973193de81ac9ffb2aab2f4d481
BRL4528 <blgc.sgo@hotmail.com>
2020-06-14 18:13:16 -0400

    update sevret key

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -6,30 +6,30 @@ import User from '../schemas/User';
 
 class SessionController {
   async store(req, res) {
-   try {
-    const { email, password } = req.body;
+    try {
+      const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+      const user = await User.findOne({ email });
 
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-      return res.status(401).json({ error: 'Password does not match' });
+      if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ error: 'Password does not match' });
+      }
+
+      const { _id, name } = user;
+
+      return res.json({
+        user: {
+          _id,
+          name,
+          email,
+        },
+        token: jwt.sign({ _id }, authConfig.secret, {
+          expiresIn: authConfig.expiresIn,
+        }),
+      });
+    } catch (error) {
+      return res.status(500).send(`Error authenticating${error}`);
     }
-
-    const { _id, name } = user;
-
-    return res.json({
-      user: {
-        _id,
-        name,
-        email,
-      },
-      token: jwt.sign({ _id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }),
-    });
-   } catch(error) {
-     return res.status(500).send('Error authenticating' + error);
-   }
   }
 }
 
--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: process.env.APP_SECRET,
+  secret: 'softwarsetwobrother',
   expiresIn: '7d',
 };
